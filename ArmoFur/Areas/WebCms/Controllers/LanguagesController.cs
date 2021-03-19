149e0a96189c6eff04d5936a39626263bd57a896
FRENK SCOFIELD <kam_alibeyli@mail.ru>
2021-03-19 16:59:27 +0400

    03/19/2021 -16:35- Having clad pages.

--- ArmoFur/Areas/WebCms/Controllers/LanguagesController.cs
+++ ArmoFur/Areas/WebCms/Controllers/LanguagesController.cs
@@ -89,5 +89,68 @@ namespace ArmoFur.Areas.WebCms.Controllers
         }
         //Language create page End
 
+        //Language edit page Start
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var language = await _context.Languages.FindAsync(id);
+            if (language == null)
+            {
+                return NotFound();
+            }
+            return View(language);
+        }
+        //Post Start
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Language language)
+        {
+            if (id != language.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(language);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LanguageExists(language.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(language);
+        }
+
+        private bool LanguageExists(int id)
+        {
+            return _context.Languages.Any(e => e.Id == id);
+        }
+        //Language edit page End
+
+        //Language delete page Start
+        public async Task<IActionResult> Delete(int id)
+        {
+            var language = await _context.Languages.FindAsync(id);
+            _context.Languages.Remove(language);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        //Language delete page End
     }
 }
---
+++ ArmoFur/Areas/WebCms/Controllers/MainHomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ArmoFur.Models.DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArmoFur.Areas.WebCms.Controllers
+{
+    [Area("WebCms")]
+    //[Route("WebCms/")]
+    [Route("WebCms/[controller]/[action]")]
+    public class MainHomeController : Controller
+    {
+        private readonly MyContext _context;
+
+        public MainHomeController(MyContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
